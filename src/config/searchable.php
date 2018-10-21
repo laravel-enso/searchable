61f40c52937698d277482f0f03587b2f73d931b9
mishu <mishu@xtelecom.ro>
2018-10-21 15:09:51 +0300

    minor refactor

--- src/config/searchable.php
+++ src/config/searchable.php
@@ -1,9 +1,6 @@
 <?php
 
-use LaravelEnso\Teams\app\Models\Team;
 use LaravelEnso\People\app\Models\Person;
-use LaravelEnso\Contacts\app\Models\Contact;
-use LaravelEnso\HowToVideos\app\Models\Video;
 
 return [
     'defaultLabel' => 'name',
@@ -19,26 +16,6 @@ return [
             'attributes' => ['name', 'appellative', 'email', 'phone'],
             'label' => 'name',
             'permissionGroup' => 'administration.people',
-        ],
-        Team::class => [
-            'group' => 'Team',
-            'attributes' => ['name'],
-            'label' => 'name',
-            'permissionGroup' => 'administration.teams',
-        ],
-        Video::class => [
-            'group' => 'HowToVideo',
-            'attributes' => ['name', 'description'],
-            'label' => 'name',
-            'permissions' => ['index'],
-            'permissionGroup' => 'howTo.videos',
-        ],
-        Contact::class => [
-            'group' => 'Contact',
-            'attributes' => ['first_name', 'last_name'],
-            'label' => 'fullName',
-            'permissions' => ['index'],
-            'permissionGroup' => 'core.contacts',
         ],
     ],
 ];
