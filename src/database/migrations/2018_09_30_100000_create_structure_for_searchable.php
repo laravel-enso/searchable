deab77133f790efbf6f385072a009ef5ebc76b2a
mishu <mishu@xtelecom.ro>
2018-10-25 10:25:38 +0300

    updates structure migration

--- src/database/migrations/2018_09_30_100000_create_structure_for_searchable.php
+++ src/database/migrations/2018_09_30_100000_create_structure_for_searchable.php
@@ -4,10 +4,6 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 
 class CreateStructureForSearchable extends StructureMigration
 {
-    protected $permissionGroup = [
-        'name' => 'core.search', 'description' => 'System Search permissions group',
-    ];
-
     protected $permissions = [
         ['name' => 'core.search.index', 'description' => 'Search index', 'type' => 0, 'is_default' => true],
     ];
