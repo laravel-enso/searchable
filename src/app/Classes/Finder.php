720c616b6b39071c6c3e448f94ff6e850c1a3b2a
mishu <mishu@xtelecom.ro>
2018-10-26 16:43:01 +0300

    improved searchable to search on relations

--- src/app/Classes/Finder.php
+++ src/app/Classes/Finder.php
@@ -2,6 +2,8 @@
 
 namespace LaravelEnso\Searchable\app\Classes;
 
+use Illuminate\Support\Str;
+
 class Finder
 {
     private $words;
@@ -22,6 +24,7 @@ class Finder
         $this->models->keys()
             ->each(function ($model) {
                 $results = $this->query($model);
+
                 if ($results->isNotEmpty()) {
                     $this->results = $this->results->merge(
                         $this->map($results, $model)
@@ -36,45 +39,66 @@ class Finder
     {
         return $model::where(function ($query) use ($model) {
             $this->words->each(function ($word) use ($query, $model) {
-                collect($this->models[$model]['attributes'])
-                    ->each(function ($attribute) use ($query, $model, $word) {
-                        $query->orWhere($attribute, 'like', '%'.$word.'%');
-
-                        collect($this->scopes($model))
-                            ->each(function ($scope) use ($query) {
-                                $query->{$scope}();
-                            });
-                    });
+                $this->match($model, $query, $word);
             });
-        })
-        ->limit($this->limit())
+
+            $this->addScopes($model, $query);
+        })->limit($this->limit())
         ->get();
+    }
+
+    private function match($model, $query, $word)
+    {
+        $this->attributes($model)
+            ->each(function ($attribute) use ($query, $model, $word) {
+                $this->isNested($attribute)
+                    ? $this->where($query, $attribute, $word)
+                    : $query->orWhere($attribute, 'like', '%'.$word.'%');
+            });
+    }
+
+    private function where($query, $attribute, $word)
+    {
+        if ($this->isNested($attribute)) {
+            $attributes = collect(explode('.', $attribute));
+
+            $query->orWhere(function ($query) use ($attributes, $word) {
+                $query->whereHas($attributes->shift(), function ($query) use ($attributes, $word) {
+                    $this->where($query, $attributes->implode('.'), $word);
+                });
+            });
+
+            return;
+        }
+
+        $query->where($attribute, 'like', '%'.$word.'%');
+    }
+
+    private function addScopes($model, $query)
+    {
+        $this->scopes($model)
+            ->each(function ($scope) use ($query) {
+                $query->{$scope}();
+            });
     }
 
     private function map($results, $model)
     {
         return $results->map(function ($result) use ($model) {
             return [
-                'id' => $result->getKey(),
+                'param' => [$this->routeParam($model) => $result->getKey()],
                 'group' => $this->group($model),
                 'label' => $result->{$this->label($model)},
-                'routes' => $this->routes($model),
+                'routes' => $this->actions($model),
             ];
         });
     }
 
-    private function routes($model)
+    private function actions($model)
     {
-        $routes = collect(
-            $this->models[$model]['permissions']
-                ?? $this->routes->keys()
-            )->map(function ($route) use ($model) {
-                return $this->models[$model]['permissionGroup'].'.'.$route;
-            });
-
         return auth()->user()->role
             ->permissions()
-            ->whereIn('name', $routes)
+            ->whereIn('name', $this->routes($model))
             ->pluck('name')
             ->sortBy(function ($route) {
                 return $this->routes->keys()
@@ -91,7 +115,32 @@ class Finder
     private function words($query)
     {
         return collect(explode(' ', trim($query)))
-            ->filter();
+        ->filter();
+    }
+
+    private function attributes($model)
+    {
+        return collect($this->models[$model]['attributes']);
+    }
+
+    private function label($model)
+    {
+        return $this->models[$model]['label']
+            ?? config('enso.searchable.defaultLabel');
+    }
+
+    private function routeParam($model)
+    {
+        return Str::camel(class_basename($model));
+    }
+
+    private function routes($model)
+    {
+        return collect(
+                $this->models[$model]['permissions'] ?? $this->routes->keys()
+            )->map(function ($route) use ($model) {
+                return $this->models[$model]['permissionGroup'].'.'.$route;
+            });
     }
 
     private function group($model)
@@ -103,12 +152,6 @@ class Finder
                 })->implode(' ');
     }
 
-    private function label($model)
-    {
-        return $this->models[$model]['label']
-            ?? config('enso.searchable.defaultLabel');
-    }
-
     private function icon($route)
     {
         return $this->routes[$this->suffix($route)] ?? null;
@@ -116,8 +159,7 @@ class Finder
 
     private function suffix($route)
     {
-        return collect(explode('.', $route))
-            ->last();
+        return collect(explode('.', $route))->last();
     }
 
     private function limit()
@@ -127,7 +169,11 @@ class Finder
 
     private function scopes($model)
     {
-        return $this->models[$model]['scopes']
-            ?? [];
+        return collect($this->models[$model]['scopes'] ?? []);
+    }
+
+    private function isNested($attribute)
+    {
+        return Str::contains($attribute, '.');
     }
 }
