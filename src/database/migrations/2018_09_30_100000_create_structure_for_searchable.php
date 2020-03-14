c278a7104225c56725f11eeb71d38818a8e380b0
mishu <mishu@xtelecom.ro>
2020-03-14 18:38:07 +0200

    removes Types from migration

--- src/database/migrations/2018_09_30_100000_create_structure_for_searchable.php
+++ src/database/migrations/2018_09_30_100000_create_structure_for_searchable.php
@@ -1,11 +1,10 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForSearchable extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.search.index', 'description' => 'Search index', 'type' => Types::Read, 'is_default' => true],
+        ['name' => 'core.search.index', 'description' => 'Search index', 'is_default' => true],
     ];
 }
