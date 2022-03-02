bebc9aa9c697e468335b67e44d3d43310c8952ec
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 14:46:52 +0200

    updated migration

--- database/migrations/2018_09_30_100000_create_structure_for_searchable.php
+++ database/migrations/2018_09_30_100000_create_structure_for_searchable.php
@@ -2,9 +2,9 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForSearchable extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'core.search.index', 'description' => 'Search index', 'is_default' => true],
     ];
-}
+};
